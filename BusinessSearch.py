416c2728ae57b6271ec192169debc91f487db486
ZackP-Coyote <127189971+ZackP-Coyote@users.noreply.github.com>
2023-10-25 15:04:36 -0700

    Able to get business params

--- BusinessSearch.py
+++ BusinessSearch.py
@@ -13,9 +13,11 @@ API_KEY = get_my_key()
 ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
 Headers = {'Authorization': 'bearer %s' % API_KEY}
 
+
+
 #define parameters
 PARAMETERS = {'term':'coffee',                         #Term is the keyword for the lookup
-              'limit':5,                               #How many businesses
+              'limit':50,                               #How many businesses
               'radius':10000,                            #Radius is in Meters
               'location': 'San Bernardino'}            #TODO: Change all of these for vals relative to the user
 
@@ -23,9 +25,43 @@ PARAMETERS = {'term':'coffee',                         #Term is the keyword for 
 
 response = requests.get(url = ENDPOINT, params= PARAMETERS, headers= Headers)
 
+if response.status_code == 200:
+    data = response.json()
+    businesses = data.get('businesses', [])
+
+    for business in businesses:
+        print(f"Name: {business['name']}")
+        print(f"Business ID: {business['id']}")
+        print(f"Is Closed: {business['is_closed']}")
+        print(f"Rating: {business['rating']}")
+        print(f"Image: {business['image_url']}")
+        # print(f": {business['']}")
+
+        #location 
+        location = business['location']
+        address = location.get('address1', '')
+        city = location.get('city', '')
+        state = location.get('state', '')
+        zipCode = location.get('zip_code', '')
+
+        full_address = f'Address: {address}, {city}, {state}, {zipCode}'
+        print(full_address)
+
+
+        print("")
+else:
+    print(f"Error")
+
+
+
+
+
+
+
+
 #convert JSON from yelp to python
-business_data = response.json()
-print(business_data)
+# business_data = response.json()
+# print(business_data)
 
 
 #Full List of Params
