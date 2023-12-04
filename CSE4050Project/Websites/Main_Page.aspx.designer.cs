13948755713d87038aaf11a001302eec14d66058
Yun <138333327+Yun-Seo1@users.noreply.github.com>
2023-12-04 15:58:54 -0800

    Fix main page crash

--- CSE4050Project/Websites/Main_Page.aspx.designer.cs
+++ CSE4050Project/Websites/Main_Page.aspx.designer.cs
@@ -15,22 +15,22 @@ namespace CSE4050Project
     {
 
         /// <summary>
-        /// TextBox3 control.
+        /// TextBox1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox3;
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
 
         /// <summary>
-        /// TextBox4 control.
+        /// TextBox2 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox4;
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
 
         /// <summary>
         /// DropDownList1 control.
