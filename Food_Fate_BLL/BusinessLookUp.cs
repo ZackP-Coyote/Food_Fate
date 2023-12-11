7221aa1ad5994496072c0f0623e123af53c958e7
ZackP-Coyote <127189971+ZackP-Coyote@users.noreply.github.com>
2023-12-11 15:43:37 -0800

    Made changes to Blookup

--- Food_Fate_BLL/BusinessLookUp.cs
+++ Food_Fate_BLL/BusinessLookUp.cs
@@ -2,10 +2,12 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using static YelpApi;
 
 class Lookup
 {
-    static async Task Main(string[] args)
+    static async Task<string[]> BL(string[] args)
     {
         // Insert your Yelp Fusion API key here
         string apiKey = GetMyKey.ApiKey();
@@ -19,19 +21,27 @@ class Lookup
             client.DefaultRequestHeaders.Add("Authorization", "Bearer " + apiKey);
 
             // Construct the request URL with the business ID
-            var request = new RestRequest(baseUrl);
-
+            var request = new RestRequest();
+            //Request info for the Api
             HttpResponseMessage response = await client.GetAsync(baseUrl);
 
-            if (response.IsSuccessStatusCode)
-            {
-                string responseContent = await response.Content.ReadAsStringAsync();
-                Console.WriteLine(responseContent); // Output the response content (JSON data)
-            }
-            else
-            {
-                Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
-            }
+            string jSonRespose = await response.Content.ReadAsStringAsync();
+            var businessDetails = JsonConvert.DeserializeObject<Business>(jSonRespose);
+
+            //get the information from the Api
+            string id = businessDetails.Id;
+            string name = businessDetails.Name;
+            string address = businessDetails.Location.Address1;
+            string rating = businessDetails.Rating.ToString();
+            string url = businessDetails.Url;
+            string image = businessDetails.Image_url;
+
+            //make into a string array
+            string[] businessInfo = { id, name, address, rating, url, image };
+
+            //return the string array
+            return businessInfo;
+
             
         }
     }
