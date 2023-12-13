77fc47fbb3ebfe15ef693b74a75d87a50b91ade3
BrandonV01 <brandonvarela77@gmail.com>
2023-12-12 19:40:33 -0800

    Fixed issue where favoriting a restaurant would reload page

--- CSE4050Project/Websites/Search_Results.aspx.cs
+++ CSE4050Project/Websites/Search_Results.aspx.cs
@@ -17,52 +17,58 @@ namespace CSE4050Project.Websites
         private string id3;
         protected void Page_Load(object sender, EventArgs e)
         {
-            //if (!IsPostBack)
-            //{
+            if (!IsPostBack)
+            {
                 
 
-                string ZipCity = Request.QueryString["city"];
-                string Radius = Request.QueryString["radius"];
-                string Filters = Request.QueryString["value"];
+                string ZipCityStr = Request.QueryString["city"];
+                string RadiusStr = Request.QueryString["radius"];
+                string FiltersStr = Request.QueryString["value"];
 
+                ZipCity.Text = ZipCityStr;
+                Radius.Text = RadiusStr;
+                Filters.SelectedValue = FiltersStr;
 
                 YelpApi yelpApi = new YelpApi();
-                string[] searcharr = { ZipCity, Radius, Filters };
-                List<string[]> businessInfo = yelpApi.BSAsync(searcharr).Result;
+                string[] searcharr = { ZipCityStr, RadiusStr, FiltersStr };
+                Session["businessInfo"] = yelpApi.BSAsync(searcharr).Result;
+            }
+            List<string[]> businessInfo = (List<string[]>)Session["businessInfo"];
+            
 
-                //fetch id, name , image, and url from businessInfo
-                id1 = businessInfo[0][0];
-                string name1 = businessInfo[0][1];
-                string imageurl1 = businessInfo[0][4];
-                string rating1 = businessInfo[0][3];
-                string url1 = businessInfo[0][5];
+            //fetch id, name , image, and url from businessInfo
+            id1 = businessInfo[0][0];
+            string name1 = businessInfo[0][1];
+            string imageurl1 = businessInfo[0][4];
+            string rating1 = businessInfo[0][3];
+            string url1 = businessInfo[0][5];
 
-                id2 = businessInfo[1][0];
-                string name2 = businessInfo[1][1];
-                string imageurl2 = businessInfo[1][4];
-                string rating2 = businessInfo[1][3];
-                string url2 = businessInfo[1][5];
+            id2 = businessInfo[1][0];
+            string name2 = businessInfo[1][1];
+            string imageurl2 = businessInfo[1][4];
+            string rating2 = businessInfo[1][3];
+            string url2 = businessInfo[1][5];
 
-                id3 = businessInfo[2][0];
-                string name3 = businessInfo[2][1];
-                string imageurl3 = businessInfo[2][4];
-                string rating3 = businessInfo[2][3];
-                string url3 = businessInfo[2][5];
+            id3 = businessInfo[2][0];
+            string name3 = businessInfo[2][1];
+            string imageurl3 = businessInfo[2][4];
+            string rating3 = businessInfo[2][3];
+            string url3 = businessInfo[2][5];
 
 
-                //Restaurant Results
-                Restaurant_Image_1.Controls.Add(new Literal() { Text = "<div> <img width=300px src=" + imageurl1 + " alt=Resturant 1 Image></div>" });
-                Restaurant_Name_1.Controls.Add(new Literal() { Text = "<div> <a href=" + url1 + ">" + name1 + "</a></div>" });
-                Restaurant_Description_1.Controls.Add(new Literal() { Text = "<div> Rating:" + rating1 + "</div>" });
+            //Restaurant Results
+            Restaurant_Image_1.Controls.Add(new Literal() { Text = "<div> <img width=300px src=" + imageurl1 + " alt=Resturant 1 Image></div>" });
+            Restaurant_Name_1.Controls.Add(new Literal() { Text = "<div> <a href=" + url1 + ">" + name1 + "</a></div>" });
+            Restaurant_Description_1.Controls.Add(new Literal() { Text = "<div> Rating:" + rating1 + "</div>" });
 
-                Restaurant_Image_2.Controls.Add(new Literal() { Text = "<div> <img width=300px src=" + imageurl2 + " alt=Resturant 2 Image></div>" });
-                Restaurant_Name_2.Controls.Add(new Literal() { Text = "<div> <a href=" + url2 + ">" + name2 + "</a></div>" });
-                Restaurant_Description_2.Controls.Add(new Literal() { Text = "<div> Rating:" + rating2 + "</div>" });
+            Restaurant_Image_2.Controls.Add(new Literal() { Text = "<div> <img width=300px src=" + imageurl2 + " alt=Resturant 2 Image></div>" });
+            Restaurant_Name_2.Controls.Add(new Literal() { Text = "<div> <a href=" + url2 + ">" + name2 + "</a></div>" });
+            Restaurant_Description_2.Controls.Add(new Literal() { Text = "<div> Rating:" + rating2 + "</div>" });
 
-                Restaurant_Image_3.Controls.Add(new Literal() { Text = "<div> <img width=300px src=" + imageurl3 + " alt=Resturant 3 Image></div>" });
-                Restaurant_Name_3.Controls.Add(new Literal() { Text = "<div> <a href=" + url3 + ">" + name3 + "</a></div>" });
-                Restaurant_Description_3.Controls.Add(new Literal() { Text = "<div> Rating:" + rating3 + "</div>" });
-            //}
+            Restaurant_Image_3.Controls.Add(new Literal() { Text = "<div> <img width=300px src=" + imageurl3 + " alt=Resturant 3 Image></div>" });
+            Restaurant_Name_3.Controls.Add(new Literal() { Text = "<div> <a href=" + url3 + ">" + name3 + "</a></div>" });
+            Restaurant_Description_3.Controls.Add(new Literal() { Text = "<div> Rating:" + rating3 + "</div>" });
+            
 
             
         }
