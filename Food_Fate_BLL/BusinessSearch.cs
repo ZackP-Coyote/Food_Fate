b1019d0124254a8bf96a2317220281a4f8b4cf3a
ZackP-Coyote <127189971+ZackP-Coyote@users.noreply.github.com>
2023-12-14 14:01:50 -0800

    deleted API key for submission

--- Food_Fate_BLL/BusinessSearch.cs
+++ Food_Fate_BLL/BusinessSearch.cs
@@ -146,6 +146,6 @@ public class GetMyKey
 {
     public static string ApiKey()
     {
-        return "rU9x88OG4P4xmlrQAiuq44l6DLl0wpVy8CKzKQeXnj72Xz086wx46zMjGwPLsgLXqZ9s4SG0LWZvV-JoS_XPGWy2HMx-psS3rXEsr9xvij8RSLevnlQRd_djeAgnZXYx";
+        return "ADD API KEY HERE";
     }
 }
